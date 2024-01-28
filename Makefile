4acbe5698e7a6f719b0ddf2422b72cd6e1e9e3ab
Burak Sekili <buraksekili@gmail.com>
2024-01-28 16:57:47 +0300

    add new flags to configure # of nodes, and enabling profiling
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
-RAFT_REPLICA ?= 1
+RAFT_REPLICACOUNT ?= 1
+RAFT_ENABLEPROFILING ?= false
 run:
-	go build -o bin/raft ./cmd && ./bin/raft --replica ${RAFT_REPLICA}
+	go build -o bin/raft ./cmd && ./bin/raft --replica=${RAFT_REPLICACOUNT} --profiling=${RAFT_ENABLEPROFILING}
--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	_ "net/http/pprof"
 
 	"github.com/buraksekili/raft"
 	"github.com/pkg/profile"
 )
 
+var (
+	numberOfNodes   = flag.Int("replica", 1, "no of raft server node")
+	enableProfiling = flag.Bool("profiling", false, "enables mem profiling")
+)
+
 func main() {
-	defer profile.Start(profile.MemProfile, profile.MemProfileRate(1), profile.ProfilePath(".")).Stop()
+	flag.Parse()
+
+	if *enableProfiling {
+		defer profile.Start(profile.MemProfile, profile.MemProfileRate(1), profile.ProfilePath(".")).Stop()
+	}
+	if *numberOfNodes == 0 {
+		fmt.Println("Running Raft with replica: ", *numberOfNodes)
+		*numberOfNodes = 1
+	}
 
 	var nodeCluster []*raft.Node
-	nodeAddrs := []string{"3000", "3001", "3002"}
+	nodeAddrs := getNodeAddrs(*numberOfNodes)
 
 	for _, nodeAddr := range nodeAddrs {
 		n := raft.NewNode(nodeAddr, nodeAddrs)
@@ -30,4 +43,12 @@ func main() {
 	}
 
 	fmt.Println("failed to start node, err: ", <-ch)
+}
+
+func getNodeAddrs(n int) (res []string) {
+	for i := 0; i < n; i++ {
+		res = append(res, fmt.Sprintf("300%d", i))
+	}
+
+	return
 }
