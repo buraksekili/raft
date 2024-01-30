66726e68797eb2243daa5e81772994cba45e4bfb
Burak Sekili <buraksekili@gmail.com>
2024-01-30 22:23:29 +0300

    cleanup tests
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- raft_test.go
+++ raft_test.go
@@ -9,6 +9,59 @@ import (
 	"github.com/buraksekili/raft/internal"
 )
 
+func TestLeaderElection(t *testing.T) {
+	tests := []struct {
+		name      string
+		nodeCount int
+	}{
+		{
+			name:      "leader election in single node cluster",
+			nodeCount: 1,
+		},
+		{
+			name:      "leader election with two nodes",
+			nodeCount: 2,
+		},
+		{
+			name:      "leader election with three nodes",
+			nodeCount: 3,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cluster := bootstrapCluster(test.nodeCount)
+
+			t.Cleanup(func() {
+				shutdown(cluster)
+			})
+
+			leaderCount := 0
+			err := internal.RetryGeneric(6, 200*time.Millisecond, func() (*rpc.Client, error) {
+				leaderCount = 0
+				for _, node := range cluster {
+					node.mu.Lock()
+					if node.state == leaderState {
+						leaderCount++
+					}
+					node.mu.Unlock()
+				}
+
+				if leaderCount != 1 {
+					return nil, fmt.Errorf("leader count expected %v got %v", 1, leaderCount)
+				}
+
+				return nil, nil
+			})
+			if err != nil {
+				t.Fatalf("failed to get correct number of leaders %v", err)
+			}
+
+		})
+	}
+
+}
+
 func getNodeAddrs(n int) (res []string) {
 	for i := 0; i < n; i++ {
 		res = append(res, fmt.Sprintf("30%d%d", n, i))
@@ -38,59 +91,6 @@ func bootstrapCluster(numberOfNodes int) []*Node {
 	return nodeCluster
 }
 
-func TestLeaderElection(t *testing.T) {
-	tests := []struct {
-		name      string
-		nodeCount int
-	}{
-		{
-			name:      "leader election in single node cluster",
-			nodeCount: 1,
-		},
-		{
-			name:      "leader election with two nodes",
-			nodeCount: 2,
-		},
-		{
-			name:      "leader election with three nodes",
-			nodeCount: 3,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			cluster := bootstrapCluster(test.nodeCount)
-
-			t.Cleanup(func() {
-				t.Log("shutting down attempt")
-				err := shutdown(cluster)
-				t.Logf("shutting down with %v", err)
-			})
-
-			leaderCount := 0
-			err := internal.RetryGeneric(6, 200*time.Millisecond, func() (*rpc.Client, error) {
-				leaderCount = 0
-				for _, node := range cluster {
-					if node.state == leaderState {
-						leaderCount++
-					}
-				}
-
-				if leaderCount != 1 {
-					return nil, fmt.Errorf("leader count expected %v got %v", 1, leaderCount)
-				}
-
-				return nil, nil
-			})
-			if err != nil {
-				t.Fatalf("failed to get correct number of leaders %v", err)
-			}
-
-		})
-	}
-
-}
-
 func shutdown(cluster []*Node) error {
 	for _, n := range cluster {
 		err := internal.RetryGeneric(3, 500*time.Millisecond,
@@ -107,15 +107,5 @@ func shutdown(cluster []*Node) error {
 		}
 	}
 
-	return internal.RetryGeneric(5, 500*time.Millisecond, func() (*rpc.Client, error) {
-		for _, n := range cluster {
-			if n.running != false {
-				return nil, fmt.Errorf("node: %v is still running", n.id)
-			}
-			if n.listener != nil {
-				return nil, fmt.Errorf("node: %v has valid listener", n.id)
-			}
-		}
-		return nil, nil
-	})
+	return nil
 }
